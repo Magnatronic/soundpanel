5c71986f0f4a2e5cca6cdc9856dee9bb8708ee7d
magnatronic <cdmcguire@gmail.com>
2025-07-09 21:33:16 +0100

    Add GitHub Pages deployment config

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+// GitHub Pages deployment config for Vite
+// See: https://vitejs.dev/guide/static-deploy.html#github-pages
+/** @type {import('vite').UserConfig} */
+export default {
+  base: '/soundpanel/'
+};
